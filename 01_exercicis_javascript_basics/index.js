1b03a365b197670211bdca56c9d114d54e3cf3d9
xxjcaxx <jocasal@gmail.com>
2024-09-17 18:46:47 +0200

    a ver si va

--- 01_exercicis_javascript_basics/index.js
+++ 01_exercicis_javascript_basics/index.js
@@ -2,7 +2,7 @@ export function createArraySomeTypes() {
     // Debe retornar un array que contenga, en este orden:
     //un número, un string, un boolean, otro array, un objeto, null, undefined, NaN y una función
     // En este primer ejecicio ponemos la solución para demostrar cómo se ha de retornar para pasar el test.
-     return [1,"aa",false,[1,2,3,4],{a:1,b:2}, null, undefined, NaN, function a(){return "a";}];  
+      return [1,"aa",false,[1,2,3,4],{a:1,b:2}, null, undefined, NaN, function a(){return "a";}];  
   }
 
 
